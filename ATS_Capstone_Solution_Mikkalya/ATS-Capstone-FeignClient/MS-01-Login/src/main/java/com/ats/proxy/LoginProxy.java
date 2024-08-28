39e7c595ac4a7598792015e5254c5963bc3135d0
Mikkalya Satheesh <mikkalya.satheesh@ascendion.com>
2024-08-28 16:20:29 +0530

    Ribbon client commit

--- ATS_Capstone_Solution_Mikkalya/ATS-Capstone-FeignClient/MS-01-Login/src/main/java/com/ats/proxy/LoginProxy.java
+++ ATS_Capstone_Solution_Mikkalya/ATS-Capstone-FeignClient/MS-01-Login/src/main/java/com/ats/proxy/LoginProxy.java
@@ -3,7 +3,7 @@ import com.ats.model.BookingDTO;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import java.util.List;
-@FeignClient(name="MS-05-Booking-FeignClient")
+@FeignClient(name="MS-05-Booking-FeignClient",url="localhost:8041")
 public interface LoginProxy {
     @GetMapping("/ats/api/booking")
     List<BookingDTO> getAllBooking();
